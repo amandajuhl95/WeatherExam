a3f4fad501b90698f8d1322a5c39e77fcbcc592c
Amanda <Manda_Hansen@hotmail.com>
2019-11-21 17:42:03 +0100

    finished test rest

--- src/test/java/rest/WeatherResourceTest.java
+++ src/test/java/rest/WeatherResourceTest.java
@@ -1,13 +1,10 @@
 package rest;
 
-import entities.Country;
 import utils.EMF_Creator;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
 import io.restassured.parsing.Parser;
 import java.net.URI;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.core.UriBuilder;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.util.HttpStatus;
@@ -20,8 +17,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import utils.EMF_Creator.DbSelector;
-import utils.EMF_Creator.Strategy;
 
 //Uncomment the line below, to temporarily disable this test
 //@Disabled
@@ -29,11 +24,9 @@ public class WeatherResourceTest {
 
     private static final int SERVER_PORT = 7777;
     private static final String SERVER_URL = "http://localhost/api";
-    private static Country c1, c2, c3, c4;
 
     static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
     private static HttpServer httpServer;
-    private static EntityManagerFactory emf;
 
     static HttpServer startServer() {
         ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
@@ -42,9 +35,6 @@ public class WeatherResourceTest {
 
     @BeforeAll
     public static void setUpClass() {
-
-        EMF_Creator.startREST_TestWithDB();
-        emf = EMF_Creator.createEntityManagerFactory(DbSelector.TEST, Strategy.DROP_AND_CREATE);
 
         httpServer = startServer();
         //Setup RestAssured
@@ -59,33 +49,15 @@ public class WeatherResourceTest {
         httpServer.shutdownNow();
     }
 
-    // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
-    //TODO -- Make sure to change the EntityClass used below to use YOUR OWN (renamed) Entity class
     @BeforeEach
     public void setUp() {
-        
-        EntityManager em = emf.createEntityManager();
-        
-        c1 = new Country("Denmark", 23424796);
-        c2 = new Country("Vietnam", 23424984);
-        c3 = new Country("Australia", 23424748);
-        c4 = new Country("Russia", 23424936);
-                
-        try {
-            em.getTransaction().begin();
-            em.createNamedQuery("Country.deleteAllRows").executeUpdate();
-            em.persist(c1);
-            em.persist(c2);
-            em.persist(c3);
-            em.persist(c4);
-            em.getTransaction().commit();
-        } finally {
-            em.close();
-        }
+        //Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
     }
 
     @AfterEach
     public void tearDown() throws Exception {
+
+        // Remove any data after each test was run
     }
 
     @Test
@@ -94,6 +66,7 @@ public class WeatherResourceTest {
         given().when().get("/weather").then().statusCode(200);
     }
 //
+
     /**
      * Test of getCountries method, of class WeatherResource.
      */
@@ -138,17 +111,18 @@ public class WeatherResourceTest {
 //        fail("The test case is a prototype.");
 //    }
 //
-//    /**
-//     * Test of getCityWeatherByDate method, of class WeatherResource.
-//     */
-//    @Test
-//    public void testGetCityWeatherByDate() {
-//         given()
-//                .contentType("application/json")
-//                .get("weather/city/" + 23424781).then()
-//                .assertThat()
-//                .statusCode(HttpStatus.OK_200.getStatusCode())
-//                .body("cityCode", hasSize(10), "name", hasSize(10));
-//    }
-    
+
+    /**
+     * Test of getCityWeatherByDate method, of class WeatherResource.
+     */
+    @Test
+    public void testGetCityWeatherByDate() {
+        given()
+                .contentType("application/json")
+                .get("weather/city/" + 368148 + "/" + 2013 + "/" + 12 + "/" + 12).then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("funnyAdvice", hasItem("Remember your umbrella today!"), "humidity", hasSize(6));
+    }
+
 }
