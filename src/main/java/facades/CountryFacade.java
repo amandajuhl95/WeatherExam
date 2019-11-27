39d106531896b85bdab7c471dd6d96e67af01939
Amalie Landt <malie@hotmail.dk>
2019-11-27 12:53:02 +0100

    threads inplementet in coloralgorithem

--- src/main/java/facades/CountryFacade.java
+++ src/main/java/facades/CountryFacade.java
@@ -77,7 +77,6 @@ public class CountryFacade extends DataFacade {
             UK.getCities().forEach((state) -> {
                 countryDTO.add(new CountryDTO(state));
             });
-            colorAlgorithm(countryDTO);
             return countryDTO;
 
         } catch (JsonSyntaxException | IOException e) {
@@ -87,31 +86,38 @@ public class CountryFacade extends DataFacade {
         }
     }
 
-    
-    private void colorAlgorithm(List<CountryDTO> countries) throws NotFoundException, IOException{
-        WeatherFacade WF = WeatherFacade.getFacade();
-        for (CountryDTO country : countries) {
-            CityDTO city = getCities(country.getCountryCode()).get(0);
-            int cityCode = city.getCityCode();
-            WeatherForecastDTO forecast = WF.getWeatherForecasts(cityCode).get(0);
-            double temp = forecast.getTemp();
-            country.setColorCode(temp);
+    public void colorAlgorithm(List<CountryDTO> countries) throws NotFoundException {
+        try {
+            ExecutorService executor = Executors.newFixedThreadPool(countries.size());
+            Queue<Future<String>> queue = new ArrayBlockingQueue(countries.size());
+
+            WeatherFacade WF = WeatherFacade.getFacade();
+            for (CountryDTO country : countries) {
+
+                Future<String> future = executor.submit(() -> {
+                    CityDTO city = getCities(country.getCountryCode()).get(0);
+                    WeatherForecastDTO forecast = WF.getWeatherForecasts(city.getCityCode()).get(0);
+                    country.setColorCode(forecast.getTemp());
+                    return country.getName();
+                });
+                
+                queue.add(future);
+            }
+            while (!queue.isEmpty()) {
+                Future<String> task = queue.poll();
+                if (!task.isDone()) {
+                    queue.add(task);
+                } 
+            }
+            executor.shutdown();
+            executor.awaitTermination(1, TimeUnit.DAYS);
+
+        } catch (InterruptedException e) {
+            throw new NotFoundException("Colorcode could not be generated");
         }
 
-        
-        
     }
-    
-    public static void main(String[] args) {
-        
-        CountryFacade CF = getFacade(EMF_Creator.createEntityManagerFactory(EMF_Creator.DbSelector.DEV, EMF_Creator.Strategy.CREATE));
-        try{
-      List<CountryDTO> contries =  CF.getCountries();
-        }catch( Exception e){
-            System.out.println("Damm dude..:  " + e.getMessage());
-        }
-        
-    }
+
     public CityDTO getCity(String cityname) throws NotFoundException {
 
         try {
